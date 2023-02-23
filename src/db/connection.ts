c6afbeb4cda25b39f90c23f695a38aaf7073dd30
Jonathan <jonathanelian64@gmail.com>
2023-02-22 21:54:52 -0600

    Add Deno.env keywords

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,9 +4,9 @@ import { load } from "dotenv/mod.ts";
 export async function connectMongo() {
   try {
     const env = await load()
-    const URI_MONGO = env.URI_MONGO ?? ""
-    const CLUSTER = env.CLUSTER ?? ""
-    const MONGO_APIKEY = env.MONGO_APIKEY ?? ""
+    const URI_MONGO = env.URI_MONGO ?? Deno.env.get("URI_MONGO")
+    const CLUSTER = env.CLUSTER ?? Deno.env.get("CLUSTER")
+    const MONGO_APIKEY = env.MONGO_APIKEY ?? Deno.env.get("MONGO_APIKEY")
 
     const client = new MongoClient({
       endpoint: URI_MONGO,
