32d3e5c74aef08fbee18ca6d87d4ca588df70bab
Jonathan <jonathanelian64@gmail.com>
2023-03-04 20:44:51 -0700

    Add json of questions and scrape html local

---
+++ src/utils/scrape.ts
@@ -0,0 +1,27 @@
+import { cheerio } from "https://deno.land/x/cheerio@1.0.6/mod.ts";
+
+export async function getQuestions() {
+
+  const fileRaw = await Deno.readTextFile(`${Deno.cwd()}/html/questions.html`)
+
+  const $ = cheerio.load(fileRaw)
+
+  const questions = []
+  $('.question').each((idx, el) => {
+    const title = $(el).children('.title').text()
+    const id = $(el).children('.id').text()
+    const code = $(el).children('.code').text()
+    const url = `/api/questions/${id}`
+
+    questions.push({
+      title,
+      id,
+      code,
+      url
+    })
+  })
+
+  Deno.writeTextFile(`${Deno.cwd()}/src/data/questions.json`, JSON.stringify(questions, null, `\t`))
+}
+
+getQuestions();
