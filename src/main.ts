3ab06a98b77f2204c5e2ce95ff5ad1a85e784f18
Jonathan <jonathanelian64@gmail.com>
2023-03-07 12:31:21 -0700

    add cors policy

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
-import { logger } from "hono_middleware"
+import { logger, cors } from "hono_middleware"
 import { serve } from "std/http/server.ts";
 import apiRouter from '$/api.ts';
-
 
 const app = new Hono()
 
 app.use("*", logger());
+
+app.use("*", cors());
 
 app.get("/", (c) => c.json({
   hello: 'world'
